f9b8e4dba17b2910b13aff9dc94ff0900f6e91c2
nithishnk <nithish@nvestbank.com>
2019-05-31 15:42:03 +0530

    dcc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import Sectionone from '../src/sectionone/Sectionone'
+import Service from '../src/Service/Service'
 export default class App extends Component {
   render() {
     return (
       <div>
+      
         <Sectionone/>
       </div>
     )
